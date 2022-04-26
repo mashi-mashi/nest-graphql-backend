23b96178d18dcdffd475f32b6b4d2491677fa424
mashi-mashi <mashi.07.st@gmail.com>
2022-04-26 21:36:21 +0900

    Add migrations

---
+++ prisma/migrations/20220426123557_init/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL DEFAULT uuid_generate_v4(),
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "articles" (
+    "id" TEXT NOT NULL DEFAULT uuid_generate_v4(),
+    "providerId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "siteUrl" TEXT,
+    "type" TEXT NOT NULL,
+    "creator" TEXT,
+    "summary" TEXT,
+    "url" TEXT,
+    "imageUrl" TEXT,
+    "publishTimestamp" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "articles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "providers" (
+    "id" TEXT NOT NULL DEFAULT uuid_generate_v4(),
+    "feedUrl" TEXT NOT NULL,
+    "title" TEXT,
+    "description" TEXT,
+    "url" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "providers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "articles" ADD CONSTRAINT "articles_providerId_fkey" FOREIGN KEY ("providerId") REFERENCES "providers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
